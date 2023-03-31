2e67325fad80d2207c0ae74989773b1fdaf2cbe5
Ady Rendusara <hadyanrendusara@yahoo.com>
2023-03-31 00:39:26 -0400

    Create initial content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentBoxComponent } from './modules/content-box/content-box.component';
+import { NavbarComponent } from './modules/navbar/navbar.component';
+import { ContentDisplayComponent } from './modules/content-display/content-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentBoxComponent,
+    NavbarComponent,
+    ContentDisplayComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modules/content-box/content-box.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'content-box',
+  templateUrl: './content-box.component.html',
+  styleUrls: ['./content-box.component.scss']
+})
+export class ContentBoxComponent {
+
+}
